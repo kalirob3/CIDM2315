6b9959cc9df56409a0c78e35cf695137ffe31d42
kalirob3 <kroberts2@buffs.wtamu.edu>
2025-01-26 16:18:10 -0600

    Homework1 code file

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5; // gives us the value of x
+        double y = 3.3; // gives us the value of y
+
+        double z = 4 * x * x + 3 * y; //gives us the formula to run
+
+        Console.WriteLine($"x = {x}, y = {y}"); // gives us the line that shows the value of x & y
+        Console.WriteLine($"The value of z is: {z}"); // gives us the line that runs the formula above and shows value of z
+
+    }
+}
