4ec7fc611db6c0a55d8112dff72e1e29f20e135e
kalirob3 <kroberts2@buffs.wtamu.edu>
2025-03-24 20:12:14 -0500

    Homework7

---
+++ Homework7.cs
@@ -0,0 +1,71 @@
+﻿namespace Homework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Alice & Bob
+            Customer customer1 = new Customer(110, "Alice", 28);
+            Customer customer2 = new Customer(111, "Bob", 30);
+
+            // Print customer info
+            customer1.PrintCusInfo();
+            customer2.PrintCusInfo();
+
+            // Update customer IDs
+            customer1.ChangeID(220);
+            customer2.ChangeID(221);
+
+            // Print updated customer info
+            customer1.PrintCusInfo();
+            customer2.PrintCusInfo();
+
+            // Compare ages and print older customer’s name
+            customer1.CompareAge(customer2);
+        }
+    }
+
+    class Customer
+    {
+        private int cus_id;
+        public string cus_name;
+        public int cus_age;
+
+        
+        public Customer(int cus_id, string cus_name, int cus_age)
+        {
+            this.cus_id = cus_id;
+            this.cus_name = cus_name;
+            this.cus_age = cus_age;
+        }
+
+        // Method to change customer ID
+        public void ChangeID(int new_id)
+        {
+            cus_id = new_id;
+        }
+
+        // Method to print customer info
+        public void PrintCusInfo()
+        {
+            Console.WriteLine($"Customer: {cus_id}, name: {cus_name}, age:{cus_age}");
+        }
+
+        // Method to compare ages
+        public void CompareAge(Customer objCustomer)
+        {
+            if (this.cus_age > objCustomer.cus_age)
+            {
+                Console.WriteLine($"{this.cus_name} is older");
+            }
+            else if (this.cus_age < objCustomer.cus_age)
+            {
+                Console.WriteLine($"{objCustomer.cus_name} is older");
+            }
+            else
+            {
+                Console.WriteLine("Both customers are the same age");
+            }
+        }
+    }
+}
